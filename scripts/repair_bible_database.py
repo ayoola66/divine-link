eba18d371cfab66402c1510a36eb662c7bba10a4
Ayo Ogunrekun <ayoola66@gmail.com>
2026-01-22 10:54:04 +0000

    Fix Bible repair script book name mappings (remove hyphens)

--- scripts/repair_bible_database.py
+++ scripts/repair_bible_database.py
@@ -30,12 +30,12 @@ TRANSLATIONS = {
 BOOK_NAMES = {
     "Genesis": "genesis", "Exodus": "exodus", "Leviticus": "leviticus",
     "Numbers": "numbers", "Deuteronomy": "deuteronomy", "Joshua": "joshua",
-    "Judges": "judges", "Ruth": "ruth", "1 Samuel": "1-samuel",
-    "2 Samuel": "2-samuel", "1 Kings": "1-kings", "2 Kings": "2-kings",
-    "1 Chronicles": "1-chronicles", "2 Chronicles": "2-chronicles",
+    "Judges": "judges", "Ruth": "ruth", "1 Samuel": "1samuel",
+    "2 Samuel": "2samuel", "1 Kings": "1kings", "2 Kings": "2kings",
+    "1 Chronicles": "1chronicles", "2 Chronicles": "2chronicles",
     "Ezra": "ezra", "Nehemiah": "nehemiah", "Esther": "esther",
     "Job": "job", "Psalms": "psalms", "Proverbs": "proverbs",
-    "Ecclesiastes": "ecclesiastes", "Song of Solomon": "song-of-solomon",
+    "Ecclesiastes": "ecclesiastes", "Song of Solomon": "songofsolomon",
     "Isaiah": "isaiah", "Jeremiah": "jeremiah", "Lamentations": "lamentations",
     "Ezekiel": "ezekiel", "Daniel": "daniel", "Hosea": "hosea",
     "Joel": "joel", "Amos": "amos", "Obadiah": "obadiah",
@@ -43,15 +43,15 @@ BOOK_NAMES = {
     "Habakkuk": "habakkuk", "Zephaniah": "zephaniah", "Haggai": "haggai",
     "Zechariah": "zechariah", "Malachi": "malachi",
     "Matthew": "matthew", "Mark": "mark", "Luke": "luke", "John": "john",
-    "Acts": "acts", "Romans": "romans", "1 Corinthians": "1-corinthians",
-    "2 Corinthians": "2-corinthians", "Galatians": "galatians",
+    "Acts": "acts", "Romans": "romans", "1 Corinthians": "1corinthians",
+    "2 Corinthians": "2corinthians", "Galatians": "galatians",
     "Ephesians": "ephesians", "Philippians": "philippians",
-    "Colossians": "colossians", "1 Thessalonians": "1-thessalonians",
-    "2 Thessalonians": "2-thessalonians", "1 Timothy": "1-timothy",
-    "2 Timothy": "2-timothy", "Titus": "titus", "Philemon": "philemon",
-    "Hebrews": "hebrews", "James": "james", "1 Peter": "1-peter",
-    "2 Peter": "2-peter", "1 John": "1-john", "2 John": "2-john",
-    "3 John": "3-john", "Jude": "jude", "Revelation": "revelation"
+    "Colossians": "colossians", "1 Thessalonians": "1thessalonians",
+    "2 Thessalonians": "2thessalonians", "1 Timothy": "1timothy",
+    "2 Timothy": "2timothy", "Titus": "titus", "Philemon": "philemon",
+    "Hebrews": "hebrews", "James": "james", "1 Peter": "1peter",
+    "2 Peter": "2peter", "1 John": "1john", "2 John": "2john",
+    "3 John": "3john", "Jude": "jude", "Revelation": "revelation"
 }
 
 # Expected verse counts per book (canonical)
